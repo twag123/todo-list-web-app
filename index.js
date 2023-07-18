4cb18743cc782fb6fe7861c1c68d329bb75a303c
twag123 <tylerawagner123@gmail.com>
2023-07-18 16:28:42 -0500

    Add heading and list
    - Add heading element showing the date
    - Add list container and begin on list construction
    - Conditional formatting to make border-radius respond to any number of items

--- index.js
+++ index.js
@@ -12,8 +12,44 @@ const port = 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+
+function getDate() {
+    var days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'];
+
+    var months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'];
+
+    var dateObj = new Date();
+
+    var dayName = days[dateObj.getDay()];
+    var month = months[dateObj.getMonth()];
+    var dayNumb = dateObj.getDate().toString();
+
+    return (dayName + ', ' + month + ' ' + dayNumb);
+}
+
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {
+        date: getDate()
+    });
 });
 
 
