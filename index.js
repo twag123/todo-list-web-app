80c1f3149df62b1f31bbde98fadf4a6ed7802a38
twag123 <tylerawagner123@gmail.com>
2023-07-19 11:28:15 -0500

    Add work page functionality
    - Add work page
    - Add separate requests for today and work pages
    - Change 'home' page to 'today'
    - Move HTML heading items into the header.ejs partial

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 var homeList = [];
+var workList = [];
 
 // Get current date as a formatted string
 function getDate() {
@@ -50,22 +51,40 @@ function getDate() {
 
 // Root get
 app.get("/", (req, res) => {
-    // Render the page
+    // Render home page
     res.render("index.ejs", {
         date: getDate(),
-        itemList: homeList
+        itemList: homeList,
+        page: "/"
     });
+});
+
+app.get("/work", (req, res) => {
+    // Render the work page
+    res.render("work.ejs", {
+        date: getDate(),
+        itemList: workList,
+        page: "/work"
+    })
 });
 
 // Root post
 app.post("/", (req, res) => {
-    // Add new item to the list
+    // Add new item to today's list
     homeList.push(req.body.newItem);
     
     // Redirect to root get
     res.redirect("/");
 });
 
+// Work post
+app.post("/work", (req, res) => {
+    // Add new item to the work list
+    workList.push(req.body.newItem);
+
+    // Redirect to work get
+    res.redirect("/work");
+});
 
 
 app.listen(port, () => {
